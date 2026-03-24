39af631aa24116cb78ed44978a4511c1f83f7535
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-24 15:13:19 +0300

    implemented endpoints

--- ScheduledJobsApi/src/main/java/com/example/ScheduledJobsApi/controller/CouponController.java
+++ ScheduledJobsApi/src/main/java/com/example/ScheduledJobsApi/controller/CouponController.java
@@ -1,4 +1,61 @@
 package com.example.ScheduledJobsApi.controller;
 
+import com.example.ScheduledJobsApi.entity.Coupon;
+import com.example.ScheduledJobsApi.exception.ErrorStructureException;
+import com.example.ScheduledJobsApi.service.CouponService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/api/coupons")
 public class CouponController {
+
+
+    private final CouponService couponService;
+
+    public CouponController(CouponService couponService){
+        this.couponService = couponService;
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postBook(@RequestBody @Valid Coupon coupon){
+
+        couponService.save(coupon).getOrElseThrow(ErrorStructureException::new);
+
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Coupon> getAllPages(@RequestParam(defaultValue = "0") int page,
+                                    @RequestParam(defaultValue = "0") int size,
+                                    @RequestParam(defaultValue = "0") String sortBy,
+                                    @RequestParam(defaultValue = "0") boolean ascending){
+
+        Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return couponService.findAll(pageable);
+
+    }
+
+
+    @GetMapping("/active")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Coupon> getAllActivePages(
+                                          @RequestParam(defaultValue = "0") int page,
+                                          @RequestParam(defaultValue = "0") int size,
+                                          @RequestParam(defaultValue = "0") String sortBy,
+                                          @RequestParam(defaultValue = "0") boolean ascending){
+        Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return couponService.findAllActive(pageable);
+    }
+
+
+
 }
