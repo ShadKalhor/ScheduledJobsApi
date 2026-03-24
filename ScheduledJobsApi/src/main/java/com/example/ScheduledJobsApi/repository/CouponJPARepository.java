ca14a76b81dc616b786b1de150c9ee249ba5ffba
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-24 15:19:21 +0300

    changed method name

--- ScheduledJobsApi/src/main/java/com/example/ScheduledJobsApi/repository/CouponJPARepository.java
+++ ScheduledJobsApi/src/main/java/com/example/ScheduledJobsApi/repository/CouponJPARepository.java
@@ -1,9 +1,16 @@
 package com.example.ScheduledJobsApi.repository;
 
 import com.example.ScheduledJobsApi.entity.Coupon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponJPARepository extends JpaRepository<Coupon, UUID> {
+    Page<Coupon> findByActiveTrue(Pageable pageable);
+    List<Coupon> findByExpiryDateTimeBeforeAndActiveTrue(LocalDateTime now);
+
 }
