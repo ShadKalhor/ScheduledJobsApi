bb73e963f31803a9dce98e0eff8a92b7004be66a
shadKalhor <shad.jalal.aziz@gmail.com>
2026-03-24 11:42:37 +0300

    init

---
+++ ScheduledJobsApi/src/main/java/com/example/ScheduledJobsApi/ScheduledJobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScheduledJobsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledJobsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledJobsApiApplication.class, args);
+	}
+
+}
---
+++ ScheduledJobsApi/src/test/java/com/example/ScheduledJobsApi/ScheduledJobsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScheduledJobsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledJobsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
